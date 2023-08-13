cfc90e0891dba26bfdcf65292c00cb7ec36b01ff
strurao <strurao@gmail.com>
2023-08-13 19:16:36 +0900

    Team: Search-Comment

--- Team_Search/bfs.cpp
+++ Team_Search/bfs.cpp
@@ -2,7 +2,8 @@
 #include <queue>
 using namespace std;
 
-// 기본 bfs
+// 기본 bfs 함수 정의
+// 그래프에 포함되어 있는 노드의 개수가 8개라고 한다면
 bool visited[9];
 vector<int> graph[9];
 
@@ -10,15 +11,15 @@ void bfs(int start)
 {
 	queue<int> q;
 	q.push(start);
-	visited[start] = true;
+	visited[start] = true;  // 현재 노드를 방문 처리
 
-	while (false == q.empty())
+	while (false == q.empty()) // 큐가 빌 때까지 반복
 	{
-		int x = q.front();
+		int x = q.front(); // 큐에서 하나의 원소를 뽑아 출력
 		q.pop();
 		cout << x << ' ';
-		for (int i = 0; i < graph[x].size(); ++i)
-		{
+		for (int i = 0; i < graph[x].size(); ++i)  // 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+		{ 
 			int y = graph[x][i];
 			if (false == visited[y])
 			{
@@ -95,3 +96,45 @@ int main()
 	cout << bfs(0, 0);
 	return 0;
 }
+
+/*
+// 기본 bfs에 대한 main()
+
+int main(void) {
+	// 노드 1에 연결된 노드 정보 저장
+	graph[1].push_back(2);
+	graph[1].push_back(3);
+	graph[1].push_back(8);
+
+	// 노드 2에 연결된 노드 정보 저장
+	graph[2].push_back(1);
+	graph[2].push_back(7);
+
+	// 노드 3에 연결된 노드 정보 저장
+	graph[3].push_back(1);
+	graph[3].push_back(4);
+	graph[3].push_back(5);
+
+	// 노드 4에 연결된 노드 정보 저장
+	graph[4].push_back(3);
+	graph[4].push_back(5);
+
+	// 노드 5에 연결된 노드 정보 저장
+	graph[5].push_back(3);
+	graph[5].push_back(4);
+
+	// 노드 6에 연결된 노드 정보 저장
+	graph[6].push_back(7);
+
+	// 노드 7에 연결된 노드 정보 저장
+	graph[7].push_back(2);
+	graph[7].push_back(6);
+	graph[7].push_back(8);
+
+	// 노드 8에 연결된 노드 정보 저장
+	graph[8].push_back(1);
+	graph[8].push_back(7);
+
+	bfs(1);
+}
+*/
--- Team_Search/dfs.cpp
+++ Team_Search/dfs.cpp
@@ -2,14 +2,16 @@
 #include <vector>
 using namespace std;
 
-// 기본 dfs
+// 기본 dfs 함수 정의
+// 그래프에 포함되어 있는 노드의 개수가 8개라고 한다면
 bool visited[9];
-vector<int> graph[9];
+vector<int> graph[9]; // 인접리스트로 만들 때
 
 void dfs(int x)
 {
-	visited[x] = true;
+	visited[x] = true; // 현재 노드를 방문 처리
 	cout << x << ' ';
+	// 현재 노드와 연결된 다른 노드를 재귀적으로 방문
 	for (int i = 0; i < graph[x].size(); ++i)
 	{
 		int y = graph[x][i];
@@ -78,3 +80,45 @@ int main()
 
 	return 0;
 }
+
+/*
+// 기본 dfs에 대한 main()
+
+int main(void) {
+	// 노드 1에 연결된 노드 정보 저장
+	graph[1].push_back(2);
+	graph[1].push_back(3);
+	graph[1].push_back(8);
+
+	// 노드 2에 연결된 노드 정보 저장
+	graph[2].push_back(1);
+	graph[2].push_back(7);
+
+	// 노드 3에 연결된 노드 정보 저장
+	graph[3].push_back(1);
+	graph[3].push_back(4);
+	graph[3].push_back(5);
+
+	// 노드 4에 연결된 노드 정보 저장
+	graph[4].push_back(3);
+	graph[4].push_back(5);
+
+	// 노드 5에 연결된 노드 정보 저장
+	graph[5].push_back(3);
+	graph[5].push_back(4);
+
+	// 노드 6에 연결된 노드 정보 저장
+	graph[6].push_back(7);
+
+	// 노드 7에 연결된 노드 정보 저장
+	graph[7].push_back(2);
+	graph[7].push_back(6);
+	graph[7].push_back(8);
+
+	// 노드 8에 연결된 노드 정보 저장
+	graph[8].push_back(1);
+	graph[8].push_back(7);
+
+	dfs(1);
+}
+*/
