6ff00e71ed393b708cab40867ac3c9f0b59a896e
strurao <strurao@gmail.com>
2023-08-25 16:54:16 +0900

    10869-사칙연산-230825

---
+++ BOJ_Study/io4-10869.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const* argv[]) {
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << a + b << "\n";  // µ¡¼À
+    cout << a - b << "\n";  // »¬¼À
+    cout << a * b << "\n";  // °ö¼À
+    cout << a / b << "\n";  // ³ª´°¼À (¸ò)
+    cout << a % b << "\n";  // ³ª´°¼À (³ª¸ÓÁö)
+    return 0;
+}
