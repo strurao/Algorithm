a06ac8721fdc56b8982015869a72d21cf36068fb
strurao <strurao@gmail.com>
2023-08-25 17:31:04 +0900

    10988-팰린드롬인지 확인하기-230825

--- BOJ_Study/string-10988.cpp
+++ BOJ_Study/string-10988.cpp
@@ -9,11 +9,34 @@ int main()
 
 	for (int i = 0; i < s.length() / 2; ++i) {
 		if (s[i] != s[s.length() - i - 1]) {
-			cout << "0" << endl;
+			cout << "0" << "\n"; // 팰린드롬이면 1, 아니면 0을 출력한다.
 			return 0;
 		}
 	}
 
-	cout << "1" << endl;
+	cout << "1" << "\n";
 	return 0;
 }
+
+
+
+/*
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string a,b;
+	cin>>a;
+	b=a;
+	reverse(a.begin(),a.end());//거꾸로
+	if(a==b){ //거꾸로 읽은 단어와 원본 단어와 같을때.
+		cout<<"1";
+	}
+	else{
+		cout<<"0";
+	}
+	return 0;
+}
+
+*/
