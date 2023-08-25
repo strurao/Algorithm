998df53a38fe26d4c3b0770e0ef44cbfaa5e5092
strurao <strurao@gmail.com>
2023-08-25 19:11:17 +0900

    10951-(A+B - 4)-230825

--- BOJ_Study/loop-10951.cpp
+++ BOJ_Study/loop-10951.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 int main() {
-
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	int a, b;
 	while (!(cin >> a >> b).eof()) {	// 혹은 eof() 대신 fail()을 사용해도 된다.
 		cout << a + b << "\n";
